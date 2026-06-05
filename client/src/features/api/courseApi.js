7fc069806ea6f00463113fdcd80f908f86437186
Kaushal Khadka <kaushalkhadka789@gmail.com>
2026-06-05 10:23:01 +0545

    course

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,3 +1,4 @@
+import CreateLecture from "@/pages/admin/lecture/CreateLecture";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const COURSE_API = "http://localhost:5000/api/v1/course";
@@ -38,7 +39,20 @@ export const courseApi = createApi({
                 url: `/${courseId}`,
                 method: "GET",
             })
-        })
+        }),
+        CreateLecture: builder.mutation({
+            query: ({ lectureTitle, courseId }) => ({
+                url: `/${courseId}/lecture`,
+                method: "POST",
+                body: { lectureTitle }
+            })
+        }),
+        getCourseLecture: builder.query({
+            query: (courseId) => ({
+                url: `/${courseId}/lecture`,
+                method: "GET",
+            })
+        }),
     }),
 });
-export const { useCreateCourseMutation, useGetCreatorCourseQuery, useEditCourseMutation, useGetCourseByIdQuery } = courseApi;
+export const { useCreateCourseMutation, useGetCreatorCourseQuery, useEditCourseMutation, useGetCourseByIdQuery, useCreateLectureMutation, useGetCourseLectureQuery } = courseApi;
